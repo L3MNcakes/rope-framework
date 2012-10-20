855272908bcb3aaf1d679d2844c8408a32c82a68
Brandon Soucie <brandon@insightdesigns.com>
2012-10-19 18:07:02 -0600

    Modified core Model

--- core/Model.php
+++ core/Model.php
@@ -8,322 +8,138 @@ if(!defined('ROPE_BASE_PATH')) die('You shall not pass!');
  * Class definition for Rope-Framework Model.
  *
  */
-class Rope_Model {
-    // White-list of accepted field types
-    protected $_acceptedFieldTypes = array(
-        0 => "string",
-        1 => "number",
-        2 => "boolean",
-        3 => "timestamp",
-    );
-
-    /**
-     * Protected variables that define the model
-     *
-     * RiakClient _client -- Client from Riak PHP Library
-     * Array _validTags -- Array of valid links to other objects
-     * String _bucket -- Name of bucket
-     * String _key -- Key for object
-     * Array _links -- Array that stores linked objects
-     * String _rawData -- JSON string containing the raw data of the Riak Object
-     */
+class Rope_Model 
+{
     protected $_client;
-    protected $_validTags;
-    protected $_bucket;
+    protected $_domain;
     protected $_key;
-    protected $_links = array();
-    protected $_rawData;
+    protected $_attributes;
+    protected $_links;
 
-    /**
-     * Public variables the define the model
-     *
-     * Array _fields -- An array of fields and their corresponding data
-     */
-    public $_fields = array();
+    public function __construct($key=null) {
+        $this->_client = new RiakClient(Config::get('RiakHost'),Config::get('RiakPort'));
 
-    /**
-     * Constructor sets up the RiakClient
-     */
-    function __construct() {
-        $this->_client = new RiakClient('127.0.0.1', '8091');
-    }
-
-    /**
-     * Method to set the key of the current object
-     */
-    protected function _setKey($value) {
-        $this->_key = $value;
-        $this->_fields["uid"]["data"] = $value;
-    }
-
-    /**
-     * Method to set the rawData attribute on current object
-     */
-    protected function _setRawData($data) {
-        $this->_rawData = $data;
-    }
-
-    /**
-     * Loads data from a RiakObject returned from the Riak PHP Library
-     */
-    public function loadFromRiakObj($riakObj) {
-        if(!is_object($riakObj)) $riakObj = (object) $riakObj;
-
-        $data = $riakObj->getData();
-        foreach($data as $key=>$value) {
-            if(array_key_exists($key, $this->_fields)) {
-                $this->set($key, $value);
-            }
+        if($key) {
+            $this->_key = $key;
+            $this->load();
         }
-        $this->_setRawData($riakObj);
-
-        $this->_populateLinks();
     }
 
-    /**
-     * Stores the data of the current object into the defined bucket
-     */
     public function store() {
-        if($this->_bucket != "") {
-            $bucket = $this->_client->bucket($this->_bucket);
+        if(!isset($this->_attributes) || empty($this->_attributes)) return False;
 
-            if($this->_key == "") {
-                $this->_setKey(uniqid());
-                $this->set("created", time());
-            }
+        $bucket = $this->_client->bucket($this->_domain);
+        $obj = new RiakObject($this->_client, $bucket, $this->_key);
+        $data = json_encode($this->_attributes);
+        $obj->setContentType('application/json');
+        $obj->setData($data);
 
-            $data = array();
-            foreach($this->_fields as $field) {
-                if(!in_array($field["type"], $this->_acceptedFieldTypes)) {
-                    return False;
-                } elseif($field["name"] == "uid") {
-                    $data["uid"] = $this->_key; 
-                } else {
-                    switch($field["type"]) {
-                        case "string":
-                            $data[$field["name"]] = (string) $field["data"];
-                            break;
-                        case "number":
-                            if(is_int($field["data"]))
-                                $data[$field["name"]] = (int) $field["data"];
-                            elseif(is_float($field["data"]))
-                                $data[$field["name"]] = (float) $field["data"];
-                            else
-                                $data[$field["name"]] = (string) $field["data"];
-                            break;
-                        case "boolean":
-                            $data[$field["name"]] = (bool) $field["data"];
-                            break;
-                        case "timestamp":
-                            if($field["renew"]) {
-                                $data[$field["name"]] = (string) time();
-                                $this->set($field["name"], $data[$field["name"]]);
-                            }
-                            else
-                                $data[$field["name"]] = $field["data"];
-                            break;
-                        default:
-                            $data[$field["name"]] = (string) $field["data"];
-                    }
-                }
-            }
+        foreach($this->_links as $tag=>$link) {
+            $bucket = $this->_client->bucket($link["bucket"]);
+            $linkObj = $bucket->get($link["key"]); 
 
-            if($riakObj = $bucket->get($this->_key)) {
-                $riakObj->setData($data);
-                $riakObj->setContentType("text/json");
-                $riakObj->store();
-                $this->loadFromRiakObj($riakObj);
-            } else {
-                $riakObj = $bucket->newObject($this->_key, $data);
-                $riakObj->setContentType("text/json");
-                $riakObj->store();
-                $this->loadFromRiakObj($riakObj);
-            }
-
-            return True;
+            $obj->addLink($linkObj, $tag);
         }
-        return False;
-    }
 
-    /**
-     * Deletes the object from the Riak Database
-     */
-    public function delete() {
-        if($this->_key == "")
-            return False;
+        $obj->store();
 
-        $bucket = $this->_client->bucket($this->_bucket);
-        $riakObj = $bucket->get($this->_key);
-        $riakObj->delete();
+        $this->_key = $obj->getKey();
 
         return True;
     }
 
-    /**
-     * Method to set a field on the current object
-     */
-    public function set($fieldname, $value) {
-        if(!array_key_exists($fieldname, $this->_fields)) {
-            return false;
-        }
+    public function delete() {
+        if(!isset($this->_key) || $this->_key == "") return False;
 
-        $fieldType = $this->_fields[$fieldname]["type"];
+        $bucket = $this->_client->bucket($this->_domain);
+        $obj = $bucket->get($this->_key);
+        $obj->delete();
+        $this->_key = '';
+        $this->load();
 
-        switch($fieldType) {
-            case "string":
-                $this->_fields[$fieldname]["data"] = (string) $value;
-                break;
-            case "number":
-                if(is_int($value))
-                    $this->_fields[$fieldname]["data"] = (int) $value;
-                elseif (is_float($value))
-                    $this->_fields[$fieldname]["data"] = (float) $value;
-                else
-                    $this->_fields[$fieldname]["data"] = 0;
-                break;
-            case "boolean":
-                $this->_fields[$fieldname]["data"] = (bool) $value;
-                break;
-            case "timestamp":
-                if($this->_fields[$fieldname]["renew"])
-                    $this->_fields[$fieldname]["data"] = (string) time();
-                else
-                    $this->_fields[$fieldname]["data"] = (string) $value;
-                break;
-            default:
-                $this->_fields[$fieldname]["data"] = (string) $value;
-        }
+        return True;
     }
 
-    /**
-     * Returns the data of a corresponding field
-     */
-    public function get($fieldname) {
-        if(!array_key_exists($fieldname, $this->_fields))
-            return "";
+    public function load($key=null) {
+        if($key) $this->_key = $key;
+        if(!isset($this->_key) || $this->_key == '') {
+            foreach($this->_attributes as $name=>$value) {
+                $this->_attributes[$name] = null;
+            }
 
-        return $this->_fields[$fieldname]["data"];
-    }
+            return $this;
+        }
 
-    /**
-     * Looks up an object from the Riak database
-     */
-    public function find($key) {
-        $bucket = $this->_client->bucket($this->_bucket);
-
-        $riakObj = $bucket->get($key);
-        $data = $riakObj->getData();
-        $this->_setRawData(json_encode($data));
+        $bucket = $this->_client->bucket($this->_domain);
+        $obj = $bucket->get($this->_key);
+        $data = $obj->getData();
 
         if(!$data) return False;
 
-        foreach($data as $key=>$value) {
-            if(!array_key_exists($key, $this->_fields)) continue;
-
-            $this->set($key, $value);
+        foreach($data as $key=>$val) {
+            // if(!array_key_exists($key, $this->_attributes)) continue;
+            $this->set($key, $val);
         }
 
-        $this->_key = $riakObj->getKey();
+        $this->_key = $obj->getKey();
+
+        foreach($obj->getLinks() as $link) {
+            $this->_links[$link->getTag()][] = array(
+                "bucket" => $link->getBucket(),
+                "key" => $link->getKey(),
+            );
+        }
 
         return $this;
     }
 
-    /**
-     * Returns an array of all the current linked data to this object
-     */
-    public function getAllLinks() {
-        if($this->_links == "") {
-            $this->_populateLinks();
-        }
+    public function set($key, $val) {
+        if(!isset($key) || $key == "") return False;
 
-        return $this->_links;
+        $this->_attributes[$key] = $val;
     }
 
-    /**
-     * Populates links by querying the RIAK database
-     */
-    private function _populateLinks() {
-        if($this->_key == "")
-            return False;
+    public function get($key) {
+        if(!isset($key) || $key =="") return False;
 
-        $bucket = $this->_client->bucket($this->_bucket);
-        $riakObj = $bucket->get($this->_key);
-        $links = $riakObj->getLinks();
-        foreach($links as $link) {
-            if(array_key_exists($link->getTag(), $this->_links)) {
-                $this->_links[$link->getTag()]["matches"][$link->getKey()] = $link->get()->getData();
-            } else {
-                $this->_links[$link->getTag()] = array(
-                    "name" => $link->getTag(),
-                    "bucket" => $link->getBucket(),
-                    "matches" => array(),
-                );
-
-                $this->_links[$link->getTag()]["matches"][$link->getKey()] = $link->get()->getData();
-            }
-        }
+        return isset($this->_attributes[$key]) ? $this->_attributes[$key] : '';
     }
 
-    /**
-     * Returns a specific linked object corresponding to a tagname
-     */
-    public function getLink($tagName) {
-        if(!in_array($tagName, $this->_validTags))
-            return False;
-
-        if(!array_key_exists($tagName, $this->_links))
-            $this->_populateLinks();
-
-        if(isset($this->_links[$tagName]["matches"])) {
-            return $this->_links[$tagName]["matches"];
-        } else {
-            return array();
-        }
+    public function getDomain() {
+        return $this->_domain;
     }
 
-    /**
-     * Adds a linked object with corresponding tag name
-     */
-    public function addLink($tagName, $lBucket, $lKey) {
-        if($this->_key == "" || !in_array($tagName, $this->_validTags) || $lBucket == "" || $lKey == "")
-            return False;
+    public function getKey() {
+        return $this->_key;
+    }
 
-        $bucket = $this->_client->bucket($this->_bucket);
-        $riakObj = $bucket->get($this->_key);
-        $linkBucket = $this->_client->bucket($lBucket);
-        $linkObj = $linkBucket->get($lKey);
-        $riakObj->addLink($linkObj, $tagName);
-        $riakObj->store();
-        $this->_populateLinks();
+    public function setLink($tagName, $linkObj) {
+        if(!is_object($linkObj)) return False;
+
+        $bucket = $linkObj->getDomain();
+        $key = $linkObj->getKey();
+        $this->_links[$tagName] = array(
+            'bucket' => $bucket,
+            'key' => $key,
+        );
 
         return True;
     }
 
-    /**
-     * Returns the name of the bucket for the current object
-     */
-    public function getBucket() {
-        return $this->_getBucket();
-    }
-    protected function _getBucket() {
-        return $this->_bucket;
+    public function getLink($tagName) {
+        if(isset($this->_links[$tagName])) return $this->_links[$tagName];
+
+        return False;
     }
 
-    /**
-     * Runs a query that returns all Riak Objects in defined bucket
-     */
-    public function mapGetAll($sort=false) {
-        $jsMap = "function(v) { return [v.key]; }";
+    public function getLinkAs($tagName, $className) {
+        if(!isset($this->_links[$tagName])) return False;
 
-        $result = $this->_client->add($this->getBucket())
-            ->map($jsMap);
-
-        if($sort) {
-            $result = $result->reduce("Riak.reduceSort");
+        $linkArr = array();
+        foreach($this->_links[$tagName] as $tag) {
+            $linkArr[] = new $className($tag['key']);
         }
 
-        $result = $result->run();
-
-        return $result;
+        return $linkArr;
     }
 }
